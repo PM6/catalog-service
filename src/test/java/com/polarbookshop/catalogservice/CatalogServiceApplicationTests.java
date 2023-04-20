dc846ebaa1a978fe13c2bcf43797e8c2a10a2897
patri <patrickmarkgraf@gmail.com>
2023-04-20 18:43:13 +0200

    add testcontainers

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -6,9 +6,11 @@ import com.polarbookshop.catalogservice.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
   @Autowired private WebTestClient webTestClient;
